b9fce6df564aba1ef3a2d004a76407d8a7a49494
Dorsel89 <d.dorsel@gmx.de>
2020-12-07 11:22:06 +0100

    update

--- element.cpp
+++ element.cpp
@@ -1,6 +1,6 @@
-#include "element.h"
-
-void Element::setLabel(char *l){
+#include "phyphoxBleExperiment.h"
+#include <string.h>
+void PhyphoxBleExperiment::Element::setLabel(const char *l){
 	memset(&LABEL[0], 0, sizeof(LABEL));
 	strcat(LABEL, l);
 }
--- phyphoxBle.h
+++ phyphoxBle.h
@@ -1,19 +1,25 @@
 #ifndef PHYPHOXBLE
 #define PHYPHOXBLE
+#include <stdint.h>
+#include <stdlib.h>
+static const char *phyphoxBleExperimentServiceUUID = "cddf0001-30f7-4671-8b43-5e40ba53514a";
+static const char *phyphoxBleExperimentCharacteristicUUID = "cddf0002-30f7-4671-8b43-5e40ba53514a";
+static const char *phyphoxBleExperimentControlCharacteristicUUID = "cddf0003-30f7-4671-8b43-5e40ba53514a";
 
-#ifndef DATASIZE
-#define DATASIZE 20
-#endif
+static const char *phyphoxBleDataServiceUUID = "cddf1001-30f7-4671-8b43-5e40ba53514a";
+static const char *phyphoxBleDataCharacteristicUUID = "cddf1002-30f7-4671-8b43-5e40ba53514a";
+static const char *phyphoxBleData2CharacteristicUUID = "cddf1004-30f7-4671-8b43-5e40ba53514a";
+
+static const char *phyphoxBleConfigCharacteristicUUID = "cddf1003-30f7-4671-8b43-5e40ba53514a";
+
 
 #ifndef CONFIGSIZE
 #define CONFIGSIZE 20
 #endif
-#include <stdio.h>
-#include <string.h>
-#include "phyphoxBLE_NRF52.h"
-#include <stdint.h>
 
-struct crc32
+#include "phyphoxBLE_NRF52.h"
+
+struct phyphoxBleCrc32
 {
     static void generate_table(uint32_t(&table)[256])
     {
